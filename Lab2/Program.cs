fe9315ed0bed8154494f6f11ab079c8cc70b20a7
anastasia555555 <103441373+anastasia555555@users.noreply.github.com>
2022-06-20 13:03:49 +0300

    Add files via upload

---
+++ Lab2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            List<int> list = new List<int>();
+            list.Add(5);
+        }
+    }
+}
